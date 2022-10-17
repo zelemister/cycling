2e8a71ea9b72aafe9f841124af0e76d60f241264
Daniel <daniels.daenzer@gmail.com>
2022-10-17 20:09:32 +0200

    added image_transformation_tester.py, with the purpose of trying multiple settings of image transformations out pretty quickly.
    added a red lane in the labels.txt file. It's somewhat annoying that non red bikelanes were to those lanes.

---
+++ Code/image_transformation_tester.py
@@ -0,0 +1,7 @@
+from PIL import Image
+
+
+names = ["x_1010001.png", "x_18040012.png"]
+while True:
+    value = int(input("What's the value of the transformation that should take palce?"))
+    print(value)
