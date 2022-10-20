12bfdd947c6a8048ff21998f2530e2eb4a665f0c
SteffenJw <stevyactivity@gmail.com>
2022-10-20 12:48:56 +0200

    test commit creating a basefile to make histograms

---
+++ Code/image_histogram_test.py
@@ -0,0 +1,6 @@
+# Testfile to create a coloured histogram from an image file
+# Aim: test how much info colour red alone provides about presence of cycling infrastructure
+# To-do: read image, create histogram, define appropriate colour range, count pixels within range
+
+import os
+import numpy
