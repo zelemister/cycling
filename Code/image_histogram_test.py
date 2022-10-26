d88b4e851267a8f29e1c128bbddae93101667470
SteffenJw <stevyactivity@gmail.com>
2022-10-26 12:05:48 +0200

    added a simple histogram plot for an example image

--- Code/image_histogram_test.py
+++ Code/image_histogram_test.py
@@ -4,3 +4,18 @@
 
 import os
 import numpy
+import cv2
+from PIL import Image
+from matplotlib import pyplot as plt
+
+# get image x_18040012.png from example image folder
+img = Image.open("../Example Images/x_18040012.png")
+print(img.size)
+
+#need image type converted to matrix. channel 0=blue, 1=green, 2=red
+img_cv = cv2.imread("../Example Images/x_18040012.png")
+hist_example = cv2.calcHist([img_cv],[2],None,[256],[0,256])
+#plot histogram for example image
+plt.plot(hist_example, color='r')
+plt.show()
+#relevant pixel range 175-205?
