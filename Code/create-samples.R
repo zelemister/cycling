4f3b8718f147fbf48da31296eb88b3448d0d97b1
Daniel <daniels.daenzer@gmail.com>
2022-12-05 21:38:48 +0100

    added create-samples.R so that I can use the 9000 images on the server as well

---
+++ Code/create-samples.R
@@ -0,0 +1,24 @@
+set.seed(9271755)
+
+dir.create("train")
+dir.create("test")
+file_names <- list.files("Images", full.names = TRUE)
+
+n_train <- 9e3
+train_files <- sample(file_names, n_train, replace = FALSE)
+test_files <- setdiff(file_names, train_files)
+
+train_files_new <- gsub("Images", "train", train_files)
+test_files_new <- gsub("Images", "test", test_files)
+
+for(i in seq_along(train_files)) {
+
+  file.rename(from = train_files[[i]], to = train_files_new[[i]])
+
+}
+
+for(i in seq_along(test_files)) {
+
+  file.rename(from = test_files[[i]], to = test_files_new[[i]])
+
+}
