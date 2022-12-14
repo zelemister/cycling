4c64c81943fdc93b22c44f88ca6d740fde7c85fc
Daniel <daniels.daenzer@gmail.com>
2022-12-14 16:35:45 +0100

    made some changes

--- Code/oversampler.py
+++ Code/oversampler.py
@@ -6,19 +6,15 @@ from torchvision import transforms
 import numpy.random as random
 from torchvision.utils import save_image
 image_folder = "../Data/bikelane"
-
+from transformations import *
 seed = 12345
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
 torch.cuda.manual_seed_all(seed)
 random.seed(seed)
 torch.backends.cudnn.deterministic = True
-rotate = transforms.Compose([transforms.ToTensor(),
-                             #generate a random number between 0 and 1, then multiply by 360 for a random number between 0 and 360
-                             transforms.RandomRotation(degrees=random.random(1)[0]*360),
-                             transforms.RandomHorizontalFlip(),
-                             transforms.RandomVerticalFlip()])
 
+rotate = get_transformer("rotations")
 
 factor = 10
 for root, dirs, files in os.walk(image_folder):
