bb6e3b2f9f3f58441cc5dfcf58980e590cfe1f34
SteffenJw <stevyactivity@gmail.com>
2023-01-11 15:39:24 +0100

    added new transformations could change values

--- Code/transformations.py
+++ Code/transformations.py
@@ -12,6 +12,34 @@ def get_transformer(name: str, resolution = 256):
                                           transforms.RandomHorizontalFlip(),
                                           transforms.RandomVerticalFlip(),
                                           transforms.ToPILImage()])
+    elif name == "colorJitter":
+        transformer = transforms.Compose([transforms.ToTensor(),
+                                          #could use random.random instead of fixed
+                                          #could add option hue with max interval [-0.5, 0.5]
+                                          transforms.ColorJitter(0.5, contrast=0.5, saturation=0.5)(),
+                                          transforms.ToPILImage()])
+    elif name == "gBlur":
+        transformer = transforms.Compose([transforms.ToTensor(),
+                                          # kernel size positive uneven number
+                                          transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 200))(),
+                                          transforms.ToPILImage()])
+    elif name == "all":
+        transformer = transforms.Compose([transforms.ToTensor(),
+                                          # generate a random number between 0 and 1, then multiply by 360 for a
+                                          # random number between 0 and 360
+                                          transforms.RandomRotation(degrees=random.random() * 360),
+                                          #default probability is p=0.5
+                                          transforms.RandomHorizontalFlip(),
+                                          transforms.RandomVerticalFlip(),
+                                          transforms.ColorJitter(brightness=0.5, contrast=0.5,
+                                                                 saturation=0.5)(),
+                                          transforms.GaussianBlur(kernel_size=3, sigma=(0.1, 200))(),
+                                          transforms.ToPILImage()])
+    elif name == "grayscale":
+        transformer = transforms.Compose([transforms.ToTensor(),
+                                          #can be used to test impact of colour in image
+                                          transforms.Grayscale(num_output_channels=3)(),
+                                          transforms.ToPILImage()])
     elif name == "normalize":
         transformer = transforms.Compose([
             transforms.CenterCrop(resolution),
