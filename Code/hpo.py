39988d6cc9f676ae239488009209b888272490bd
Daniel <daniels.daenzer@gmail.com>
2023-01-19 15:43:05 +0100

    hopefully hpo works right off the bat.

--- Code/hpo.py
+++ Code/hpo.py
@@ -2,7 +2,8 @@ from ConfigSpace import Configuration, ConfigurationSpace
 import numpy as np
 from smac import HyperparameterOptimizationFacade, Scenario
 from Crossvalidation import cross_validation
-
+from pathlib import Path
+P
 
 def test_train(config, seed: int=0):
     payload = {"min_epochs": 80, "max_patience": 50, "oversampling_rate": config["oversampling_rate"],
@@ -22,5 +23,6 @@ configspace = ConfigurationSpace({"oversampling_rate":(1,100),
                                   "lr":(0.00001, 0.01)
                                  })
 scenario = Scenario(configspace, deterministic=True, n_trials=50)
-smac = HyperparameterOptimizationFacade(scenario, cross_validation, output_directory = "../Results/smac3_output")
+PathPox
+smac = HyperparameterOptimizationFacade(scenario, cross_validation)
 incumbent = smac.optimize()
