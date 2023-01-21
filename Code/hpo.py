9a14e69e1764c80c95299c0061c768ff2fc818aa
Daniel <daniels.daenzer@gmail.com>
2023-01-21 21:49:23 +0100

    cheating

--- Code/hpo.py
+++ Code/hpo.py
@@ -4,7 +4,7 @@ from smac import HyperparameterOptimizationFacade, Scenario
 from Crossvalidation import cross_validation
 
 def test_train(config, seed: int=0):
-    payload = {"min_epochs": 70, "max_patience": 50, "oversampling_rate": config["oversampling_rate"],
+    payload = {"min_epochs": 70, "max_patience": 30, "oversampling_rate": config["oversampling_rate"],
                "resolution": 256, "transformation": config["transformation"], "task": "bikelane",
                "model": config["model"], "pretrained": True, "params": "full", "weights": config["weights"],
                "optimizer": config["optimizer"], "lr": config["lr"], "stages": 1, "results_folder": "../Results/smac3_runs",
