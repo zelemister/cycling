c615eb4ff61f3fa383fa1f40e4a39cbce4a72f52
Daniel <daniels.daenzer@gmail.com>
2023-02-06 15:32:09 +0100

    FilterModel

--- Code/model_loaders.py
+++ Code/model_loaders.py
@@ -1,8 +1,9 @@
 from torchvision import models
 import torch.nn as nn
 from collections import OrderedDict
+import torch
 
-def get_model(model_name:str, pretrained=True):
+def get_model(model_name:str, pretrained=True, params_path=None):
     #there are a couple ressources for finetuning a pretrained model
     #https://discuss.pytorch.org/t/how-to-perform-finetuning-in-pytorch/419
     #https://github.com/Spandan-Madan/Pytorch_fine_tuning_Tutorial
@@ -31,6 +32,42 @@ def get_model(model_name:str, pretrained=True):
         num_ftrs = model.heads.head.in_features
         model.heads = nn.Sequential(OrderedDict([('head', nn.Linear(num_ftrs, num_classes))]))
     return model
+
+class FilterModel(nn.Module):
+    def __init__(self, model, threshold:int, num_classes):
+        super(FilterModel, self).__init__()
+        #the bikelane model is not getting trained, only the rim_layer
+        for param in model.parameters():
+            param.requires_grad = False
+        self.model = model
+        self.threshold = threshold
+        self.rim_layer = nn.Linear(model.fc.in_features, num_classes)
+
+    def forward(self, x):
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
+
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+        x = self.model.layer4(x)
+
+        x = self.model.avgpool(x)
+        x = torch.flatten(x, 1)
+        y = self.model.fc(x)
+        #convert model outputs into probabilities, transpose it such that the second row becomes the list of predicitons
+        #for class 2 then take the second row
+        y = y.softmax(1).t()[1]
+        mask = y < self.threshold
+        x = self.rim_layer(x)
+        x[mask] = torch.tensor([[1, 0]], dtype=torch.float32, requires_grad=False)
+        return x
+
+    def get_trainable_params(self):
+        self.rim_layer.parameters()
+
 """
 model1 = get_model("resnet34", pretrained=True)
 model2 = get_model("resnet50", pretrained=False)
