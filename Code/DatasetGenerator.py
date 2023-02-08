6f6ad556881d7d9590e2393578a29160f089b2bb
Daniel <daniels.daenzer@gmail.com>
2023-02-08 16:38:07 +0100

    changed DatasetGenerator.py

--- Code/DatasetGenerator.py
+++ Code/DatasetGenerator.py
@@ -33,7 +33,7 @@ class load_dataset(Dataset):
             self.resolution = 224
         if os.path.exists("../Images" + "_" + str(resolution) + "/" + phase):
             self.image_folder = "../Images" + "_" + str(resolution) + "/" + phase
-
+        else: self.image_folder="Images_256"
         labels = pd.read_csv("../labeling_clean.csv")
         labels_s = pd.read_csv("../labeling_steffen.csv")
         # labels_d = pd.read_csv("../labeling_daniel.csv")
